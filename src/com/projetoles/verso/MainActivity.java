4904fe815feffa7dfdcebfc14ae60cb2393ff3fe
Abner M. C. Araujo <abner.math.c@gmail.com>
2015-06-18 01:51:38 -0300

    Ajeitado tela de criar poema

--- src/com/projetoles/verso/MainActivity.java
+++ src/com/projetoles/verso/MainActivity.java
@@ -1,17 +1,76 @@
 package com.projetoles.verso;
 
-import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
+import android.widget.TabHost.TabSpec;
 
-public class MainActivity extends Activity {
+import com.projetoles.controller.UsuarioController;
 
+public class MainActivity extends TabActivity{
+	
+	private UsuarioController mController;
+	private TabHost mTabHost;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_menu_inferior);
+		mController = new UsuarioController(this);
+		mTabHost = getTabHost();
+		
+		TabSpec homeSpec = mTabHost.newTabSpec("Home");
+		homeSpec.setIndicator("", getResources().getDrawable(R.drawable.home));
+		Intent homeIntent = new Intent(this, CriarPoemaActivity.class);
+		homeSpec.setContent(homeIntent);
+		 
+		TabSpec comporSpec = mTabHost.newTabSpec("Compor");
+		comporSpec.setIndicator("", getResources().getDrawable(R.drawable.compor));
+		Intent comporIntent = new Intent(this, CriarPoemaActivity.class);
+		comporSpec.setContent(comporIntent);
+
+		TabSpec sobreSpec = mTabHost.newTabSpec("Sobre");
+		sobreSpec.setIndicator("", getResources().getDrawable(R.drawable.sobre));
+		Intent sobreIntent = new Intent(this, CriarPoemaActivity.class);
+		sobreSpec.setContent(sobreIntent);
+
+		TabSpec buscaSpec = mTabHost.newTabSpec("Home");
+		buscaSpec.setIndicator("", getResources().getDrawable(R.drawable.busca));
+		Intent buscaIntent = new Intent(this, CriarPoemaActivity.class);
+		buscaSpec.setContent(buscaIntent);
+		
+		mTabHost.addTab(homeSpec);
+		mTabHost.addTab(comporSpec);
+		mTabHost.addTab(sobreSpec);
+		mTabHost.addTab(buscaSpec);
+
+		mTabHost.setOnTabChangedListener(new OnTabChangeListener() {
+
+			@Override
+			public void onTabChanged(String arg0) {
+
+				setTabColor(mTabHost);
+			}
+		});
+		setTabColor(mTabHost);
 	}
+
+	private void setTabColor(TabHost tabhost) {
+		for (int i = 0; i < tabhost.getTabWidget().getChildCount(); i++) {
+			tabhost.getTabWidget().getChildAt(i).setBackgroundColor(Color.parseColor("#f8f5af")); //unselected
+		}
+		if(tabhost.getCurrentTab() == 0) {
+			tabhost.getTabWidget().getChildAt(tabhost.getCurrentTab()).setBackgroundColor(Color.parseColor("#F5DA81")); //1st tab selected
+		} else {
+			tabhost.getTabWidget().getChildAt(tabhost.getCurrentTab()).setBackgroundColor(Color.parseColor("#F5DA81")); //2nd tab selected
+		}
+	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -26,9 +85,14 @@ public class MainActivity extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_logout) {
+			mController.logout();
+			Intent i = new Intent(MainActivity.this, LoginActivity.class);
+			startActivity(i);
+			finish();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
 }
