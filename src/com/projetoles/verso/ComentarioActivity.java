7835138cb75397f286bec89de49b67d9b41a3977
brunabarbosa <bruna.barbosa@ccc.ufcg.edu.br>
2015-07-14 21:33:41 -0300

    tela de comentario

--- src/com/projetoles/verso/ComentarioActivity.java
+++ src/com/projetoles/verso/ComentarioActivity.java
@@ -1,70 +1,16 @@
 package com.projetoles.verso;
 
-import com.projetoles.controller.UsuarioController;
-import com.projetoles.dao.OnRequestListener;
-import com.projetoles.model.Usuario;
-
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.TabActivity;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TabHost;
-import android.widget.TextView;
-import android.widget.TabHost.OnTabChangeListener;
-import android.widget.TabHost.TabSpec;
 
-public class ComentarioActivity extends TabActivity {
+public class ComentarioActivity extends Activity {
 
-	
-	public static TabActivity sInstance;
-	
-	private static final int MAX_PHOTO_SIZE = 600;
-	private static final int SELECT_PHOTO = 100;
-	private static final int CAMERA_REQUEST = 1888; 
-	
-	private UsuarioController mController;
-	private TabHost mTabHost;
-	private ImageView mBtnCriarPoema;
-	private ImageView mBtnPesquisar;
-	private ImageView mUserPicturePreview;
-	private ImageView mUserPicture;
-	private RelativeLayout mProfilePhotoContent;
-	private Usuario mUsuario;
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_comentario);
-		
-		sInstance = this;
-		
-		getActionBar().hide(); 
-
-		mController = new UsuarioController(this);
-		
-		mController.getLoggedUser(new OnRequestListener(this) {
-			
-			@Override
-			public void onSuccess(Object result) {
-				mUsuario = (Usuario) result;
-				setUp();
-			}
-			
-			@Override
-			public void onError(String errorMessage) {
-				finish();
-			}
-		});
 	}
 
 	@Override
@@ -84,68 +30,5 @@ public class ComentarioActivity extends TabActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-	
-	private void setUp() {
-		mTabHost = (TabHost) findViewById(android.R.id.tabhost);
-		mTabHost.setup();
-		
-		TabSpec homeSpec = mTabHost.newTabSpec("Home");
-		TabSpec comporSpec = mTabHost.newTabSpec("Compor");
-		TabSpec sobreSpec = mTabHost.newTabSpec("Sobre");
-		TabSpec buscaSpec = mTabHost.newTabSpec("Home");
-		
-		homeSpec.setIndicator("", getResources().getDrawable(R.drawable.home));
-		Intent homeIntent = new Intent(ComentarioActivity.this, PerfilActivity.class);
-		homeSpec.setContent(homeIntent);
-		 
-		comporSpec.setIndicator("", getResources().getDrawable(R.drawable.compor));
-		Intent comporIntent = new Intent(ComentarioActivity.this, CriarPoesiaActivity.class);
-		comporSpec.setContent(comporIntent);
-
-		sobreSpec.setIndicator("", getResources().getDrawable(R.drawable.sobre));
-		Intent sobreIntent = new Intent(ComentarioActivity.this, CriarPoesiaActivity.class);
-		sobreSpec.setContent(sobreIntent);
-
-		buscaSpec.setIndicator("", getResources().getDrawable(R.drawable.busca));
-		Intent buscaIntent = new Intent(ComentarioActivity.this, PesquisarActivity.class);
-		buscaSpec.setContent(buscaIntent);
-		
-		mTabHost.addTab(homeSpec);
-		mTabHost.addTab(comporSpec);
-		mTabHost.addTab(sobreSpec);
-		mTabHost.addTab(buscaSpec);
-
-		mTabHost.setOnTabChangedListener(new OnTabChangeListener() {
-
-			@Override
-			public void onTabChanged(String arg0) {
-				if (mTabHost.getCurrentTab() == 1) {
-					mBtnCriarPoema.setVisibility(View.VISIBLE);
-				} else {
-					mBtnCriarPoema.setVisibility(View.GONE);
-				}
-				if (mTabHost.getCurrentTab() == 3) {
-					mBtnPesquisar.setVisibility(View.VISIBLE);
-				} else {
-					mBtnPesquisar.setVisibility(View.GONE);
-				}
-				setTabColor(mTabHost);
-			}
-		});
-		setTabColor(mTabHost);
-		
-		
-	}
-	
-	private void setTabColor(TabHost tabhost) {
-		for (int i = 0; i < tabhost.getTabWidget().getChildCount(); i++) {
-			tabhost.getTabWidget().getChildAt(i).setBackgroundColor(Color.parseColor("#f8f5af")); //unselected
-		}
-		if(tabhost.getCurrentTab() == 0) {
-			tabhost.getTabWidget().getChildAt(tabhost.getCurrentTab()).setBackgroundColor(Color.parseColor("#F5DA81")); //1st tab selected
-		} else {
-			tabhost.getTabWidget().getChildAt(tabhost.getCurrentTab()).setBackgroundColor(Color.parseColor("#F5DA81")); //2nd tab selected
-		}
 	}
 }
