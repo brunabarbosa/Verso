030594bc3d786c64dafd12121626396dc92130ee
Tatiana Saturno <tatiana.silva@ccc.ufcg.edu.br>
2015-07-29 22:48:13 -0300

    arrumando notificacoes e colocando botao excluir

--- src/com/projetoles/verso/ListNotificacoesAdapter.java
+++ src/com/projetoles/verso/ListNotificacoesAdapter.java
@@ -4,14 +4,18 @@ import java.util.List;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.projetoles.model.CalendarUtils;
+import com.projetoles.model.ImageUtils;
 import com.projetoles.model.Notificacao;
 
 public class ListNotificacoesAdapter extends BaseAdapter {
@@ -38,23 +42,37 @@ public class ListNotificacoesAdapter extends BaseAdapter {
 	public long getItemId(int arg0) {
 		return 0;
 	}
+	
+	private void setPhoto(ImageView imview, byte[] photo) {
+		if (photo.length > 0) {
+			Bitmap bmp = BitmapFactory.decodeByteArray(photo, 0, photo.length);
+			bmp = ImageUtils.getCroppedBitmap(bmp);
+			imview.setImageBitmap(bmp);
+		} else {
+			imview.setImageResource(R.drawable.icone_foto);
+		}
+	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent){
 		if (convertView == null) {
 			LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			convertView = inflater.inflate(R.layout.list_group_notificacao, parent, false);
+			convertView = inflater.inflate(R.layout.list_group_comentario, parent, false);
 		}
 		final Notificacao n = this.mListNotificacoes.get(position);
 		if (n != null) {
+			ImageView foto = (ImageView) convertView.findViewById(R.id.userPicture);
 			TextView nome = (TextView) convertView.findViewById(R.id.mensagem);
+			TextView comentario = (TextView) convertView.findViewById(R.id.comment);
 			TextView data = (TextView) convertView.findViewById(R.id.date);
-			nome.setText(n.getTitulo().getNome() + " " + n.getMensagem());
+			nome.setText(n.getTitulo().getNome() + n.getMensagem());
+			comentario.setVisibility(View.GONE);
 			data.setText("Em " + CalendarUtils.getDataFormada(n.getDataCriacao()));
+			setPhoto(foto, n.getTitulo().getFoto());
 			nome.setOnClickListener(new OnClickListener() {
 				
 				@Override
-				public void onClick(View v) {
+				public void onClick(View arg0) {
 					Intent intent = new Intent(mContext, UserProfileActivity.class);
 					intent.putExtra("usuario", n.getTitulo());
 					mContext.startActivity(intent);
