e10bbac2c54816fa8a233c78de4381f536227e27
brunabarbosa <bruna.barbosa@ccc.ufcg.edu.br>
2015-07-30 04:16:39 -0300

    tava dando erro em perfil

--- src/com/projetoles/verso/PerfilActivity.java
+++ src/com/projetoles/verso/PerfilActivity.java
@@ -5,61 +5,28 @@ import java.util.Collections;
 import java.util.List;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.ExpandableListView.OnGroupExpandListener;
-import android.widget.TextView;
 
 import com.projetoles.controller.PoesiaController;
-import com.projetoles.controller.SeguidaController;
 import com.projetoles.controller.UsuarioController;
 import com.projetoles.dao.OnRequestListener;
 import com.projetoles.model.Poesia;
-import com.projetoles.model.Seguida;
 
 public class PerfilActivity extends Activity {
 
 	private PoesiaController mPoesiaController;
-	private SeguidaController mSeguidaController;
 	private ExpandablePoesiaAdapter mAdapter;
 	private ExpandableListView mExpListView;
 	private List<Poesia> mListPoesias;
 
-	private void getId(String id) {
-		mPoesiaController.get(id, new OnRequestListener<Poesia>(this) {
-
-			@Override
-			public void onSuccess(Poesia p) {
-				mListPoesias.add(p);
-				Collections.sort(mListPoesias);
-				mAdapter.notifyDataSetChanged();
-
-			}
-
-			@Override
-			public void onError(String errorMessage) {
-				System.out.println(errorMessage);
-			}
-		});
-	}
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_perfil);
-		
-		TextView seguidores = (TextView) findViewById(R.id.txtNumSeguidores);
-		seguidores.setOnClickListener(new Button.OnClickListener(){
-			public void onClick(View v){
-				Intent intent = new Intent(PerfilActivity.this, SeguidoresActivity.class);
-			}
-		});
 
 		mPoesiaController = new PoesiaController(this);
-		mSeguidaController = new SeguidaController(this);
 		mExpListView = (ExpandableListView) findViewById(R.id.lvExp);
 
 		// preparing list data
@@ -79,16 +46,14 @@ public class PerfilActivity extends Activity {
 
 		mExpListView.setAdapter(mAdapter);
 		for (String id : UsuarioController.usuarioLogado.getPoesias().getList()) {
-			getId(id);
-		}
-		for (String seguidaId : UsuarioController.usuarioLogado.getSeguindo().getList()) {
-			mSeguidaController.get(seguidaId, new OnRequestListener<Seguida>(this) {
+			mPoesiaController.get(id, new OnRequestListener<Poesia>(this) {
 
 				@Override
-				public void onSuccess(Seguida seguida) {
-					for (String poesiaId : seguida.getSeguido().getPoesias().getList()) {
-						getId(poesiaId);
-					}
+				public void onSuccess(Poesia p) {
+					mListPoesias.add(p);
+					Collections.sort(mListPoesias);
+					mAdapter.notifyDataSetChanged();
+
 				}
 
 				@Override
