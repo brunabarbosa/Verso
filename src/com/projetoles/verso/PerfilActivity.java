9bff3a0052605395190e251130e4257aaf6f70ca
Abner Matheus <abner.math.c@gmail.com>
2015-08-25 21:50:56 -0300

    Timeline funcionando

--- src/com/projetoles/verso/PerfilActivity.java
+++ src/com/projetoles/verso/PerfilActivity.java
@@ -4,29 +4,41 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import com.projetoles.controller.PoesiaController;
+import com.projetoles.controller.SeguidaController;
+import com.projetoles.controller.UsuarioController;
+import com.projetoles.dao.OnRequestListener;
+import com.projetoles.model.Poesia;
+import com.projetoles.model.Seguida;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ExpandableListView;
 import android.widget.ExpandableListView.OnGroupExpandListener;
 
-import com.projetoles.controller.PoesiaController;
-import com.projetoles.controller.UsuarioController;
-import com.projetoles.dao.OnRequestListener;
-import com.projetoles.model.Poesia;
-
 public class PerfilActivity extends Activity {
 
 	private PoesiaController mPoesiaController;
+	private UsuarioController mUsuarioController;
+	private SeguidaController mSeguidaController;
 	private ExpandablePoesiaAdapter mAdapter;
 	private ExpandableListView mExpListView;
 	private List<Poesia> mListPoesias;
 
+	private void addPoesia(Poesia p) {
+		mListPoesias.add(p);
+		Collections.sort(mListPoesias);
+		mAdapter.notifyDataSetChanged();
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_perfil);
 
 		mPoesiaController = new PoesiaController(this);
+		mUsuarioController = new UsuarioController(this);
+		mSeguidaController = new SeguidaController(this);
 		mExpListView = (ExpandableListView) findViewById(R.id.lvExp);
 
 		// preparing list data
@@ -45,21 +57,45 @@ public class PerfilActivity extends Activity {
 		});
 
 		mExpListView.setAdapter(mAdapter);
+		for (String id : UsuarioController.usuarioLogado.getSeguindo().getList()) {
+			mSeguidaController.get(id, new OnRequestListener<Seguida>(this) {
+
+				@Override
+				public void onSuccess(Seguida result) {
+					for (String id : result.getSeguido().getPoesias().getList()) {
+						mPoesiaController.get(id, new OnRequestListener<Poesia>(PerfilActivity.this) {
+
+							@Override
+							public void onSuccess(Poesia result) {
+								addPoesia(result);
+							}
+
+							@Override
+							public void onError(String errorMessage) {
+								System.err.println(errorMessage);
+							}
+						});
+					}
+				}
+
+				@Override
+				public void onError(String errorMessage) {
+					System.err.println(errorMessage);
+				}
+			});
+		}
 		for (String id : UsuarioController.usuarioLogado.getPoesias().getList()) {
 			mPoesiaController.get(id, new OnRequestListener<Poesia>(this) {
 
 				@Override
 				public void onSuccess(Poesia p) {
-					mListPoesias.add(p);
-					Collections.sort(mListPoesias);
-					mAdapter.notifyDataSetChanged();
-
+					addPoesia(p);
 				}
 
 				@Override
 				public void onError(String errorMessage) {
-					System.out.println(errorMessage);
-				}
+					System.err.println(errorMessage);
+				} 
 			});
 		}
 	}
